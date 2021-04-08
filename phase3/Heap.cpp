329531cfa83c2de399b47390dbce882e1fdffb4d
arhansen <arhansen@crimson.ua.edu>
2021-04-08 09:53:06 -0500

    First heap impelmentation

--- phase3/Heap.cpp
+++ phase3/Heap.cpp
@@ -1,2 +1,122 @@
+/* ==========================
+ * Binary Heap Implementation
+ * CS201-001 Spring 2021
+ * Developed by Andrew Hansen
+ * arhansen@crimson.ua.edu
+ * ===========================
+ */
 
 #include "Heap.h"
+
+// Default constructor
+Heap::Heap()
+    : _size(0)
+{
+    _array = new CDA<KEYTYPE>();
+}
+
+// Array constructor
+Heap::Heap(KEYTYPE k[], int s)
+    : _size(0)
+{
+    _array = new CDA<KEYTYPE>();
+
+    for(int i = 0; i < s; i++)
+    {
+        insert(k[i]);
+    }
+}
+
+// Destructor
+Heap::~Heap()
+{
+    delete _array;
+}
+
+// Return minimum value of heap without modification to heap
+KEYTYPE Heap::peekKey()
+{
+    // Minimum stored at beginning of array
+    return (_array->operator[](0));
+}
+
+// Remove minimum and return value
+KEYTYPE Heap::extractMin()
+{
+    // Safety checks
+    if(_size < 1) return _sentinel;
+
+    // Only one node
+    else if(_size == 1) {
+        KEYTYPE min = (_array->operator[](0));
+        _array->DelFront();
+        _size--;
+        return min;
+    }
+
+    // All other cases
+    else {
+        KEYTYPE min = (_array->operator[](0));
+        _array->operator[](0) = _array->operator[](_size - 1);
+        _size--;
+
+        ReHeapify(0);
+
+        return min;
+    }
+}
+
+// Insert key into heap
+void Heap::insert(KEYTYPE k)
+{
+    _array->operator[](_size) = k;
+
+    // Swim up
+    SwimUp(_size);
+
+    _size++;
+}
+
+// Write keys starting at root
+void Heap::printKey()
+{
+    for(int i = 0; i < _size; i++)
+    {
+        std::cout << _array->operator[](i) << " ";
+    }
+
+    std::cout << std::endl;
+}
+// Helper to swim up or down
+void Heap::SwimUp(int startIndex)
+{
+    int i = startIndex;
+
+    while(i > 0 && ( _array->operator[](GetParentIndex(i)) > _array->operator[](i) ) )
+    {
+        // Swap values
+        KEYTYPE temp = _array->operator[](i);
+        _array->operator[](i) = _array->operator[](GetParentIndex(i));
+        _array->operator[](GetParentIndex(i)) = temp;
+
+        i = GetParentIndex(i);
+    }
+}
+
+void Heap::ReHeapify(int index)
+{
+    int leftChild = GetLeftChildIndex(index);
+    int rightChild = GetRightChildIndex(index);
+    int min = index;
+
+    if(leftChild < _size && _array->operator[](leftChild) < _array->operator[](index)) min = leftChild;
+    if(rightChild < _size && _array->operator[](index) < _array->operator[](min)) min = rightChild;
+
+    if(min != index) {
+        KEYTYPE temp = _array->operator[](index);
+        _array->operator[](index) = _array->operator[](min);
+        _array->operator[](min) = temp;
+
+        ReHeapify(min);
+    }
+}
--- phase3/Heap.h
+++ phase3/Heap.h
@@ -1,3 +1,10 @@
+/* ==========================
+ * Binary Heap Implementation
+ * CS201-001 Spring 2021
+ * Developed by Andrew Hansen
+ * arhansen@crimson.ua.edu
+ * ===========================
+ */
 
 #ifndef _HEAP_H_
 #define _HEAP_H_
@@ -5,6 +12,7 @@
 #define KEYTYPE int
 
 #include <iostream>
+#include "CDA.cpp"
 
 class Heap
 {
@@ -31,7 +39,27 @@ class Heap
         void printKey();
 
     private:
-        
-        
-}
+        CDA<KEYTYPE>* _array;
+
+        KEYTYPE _sentinel;
+
+        int _size;
+
+        // Helpers to get array indices of relatives
+        int GetParentIndex(int index) { return (index / 2); }
+
+        int GetSiblingIndex(int index) {
+            if(index % 2 == 0) return index + 1;
+            else return index - 1;
+        }
+
+        int GetLeftChildIndex(int index) { return (2 * index); }
+        int GetRightChildIndex(int index) { return (2 * index) + 1; }
+
+        // Helper to swim up or down
+        void SwimUp(int startIndex);
+
+        // Helper to restore heap order during extract min
+        void ReHeapify(int index);
+};
 #endif // !_HEAP_H_
